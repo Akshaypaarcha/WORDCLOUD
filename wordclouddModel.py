f2d3d895fcc0efefc680cccbc9050e4788f082de
AKSHAY PAARCHA <112705464+Akshaypaarcha@users.noreply.github.com>
2023-05-29 23:15:21 +0530

    Update wordclouddModel.py

--- wordclouddModel.py
+++ wordclouddModel.py
@@ -9,6 +9,8 @@ import streamlit as st
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+st.image("download.png")
+
 def generate_wordcloud(text):
     wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
 
